ce4825674ef472c97580aa2558a9928833b434d5
amecogarcia26 <amecogarcia26@gmail.com>
2026-06-17 12:14:20 +0200

    segunda versión

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {res.json({mensaje: "Creando API"});});
 
+app.get("/api", (req, res) => {res.json({nombre: "yomismo", email:"yomismo@gmail.com"});});
+
 app.listen(PORT, () =>{ console.log(`Servidor iniciado en puerto ${PORT}`);});
